58ae41e7f2e101a8f082dad11672aeab529264bf
Dormammu <saomuun@mail.ru>
2022-01-13 09:15:34 +0300

    16 17

---
+++ Lesson 16 - Цикл while/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+/*
+ * Цикл while
+ */
+
+namespace Lesson_16___Цикл_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            int.TryParse(Console.ReadLine(), out int limit);
+            while (count < limit)
+            {
+                count++;
+                Console.WriteLine($"{count} Выполняем действия");
+            }
+        }
+    }
+}
