a8b9f8dd5598c8296d7ab9ba758535a48df33464
Dormammu <saomuun@mail.ru>
2022-01-23 22:32:03 +0300

    41 42 доделать

---
+++ Lesson 41 - Оператор присваивания объединения со значением NULL/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+/*
+ * Оператор присваивания объединения со значением NULL ??=
+ */
+
+namespace Lesson_41___Оператор_присваивания_объединения_со_значением_NULL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = null;
+            
+            //возвращает другую строку если получили NULL - ее длинну
+            //str ??= "def";
+
+
+            //Console.WriteLine(str.Length);
+        }
+    }
+}
---
+++ Lesson 42 - Оператор условного NULL/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+/*
+ * Оператор условного NULL ?.
+ */
+
+namespace Lesson_42___Оператор_условного_NULL
+{
+    class Program
+    {
+        static int[] GetArray()
+        {
+            int[] myArray = null;
+            return myArray;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] myArray = GetArray();
+
+            Console.WriteLine(myArray?.Sum() ?? -1);
+        }
+    }
+}
