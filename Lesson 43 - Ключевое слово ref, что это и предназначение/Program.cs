589730c0dfd60ad0564e954bb7bb2f19937aa246
Dormammu <saomuun@mail.ru>
2022-01-31 08:23:32 +0300

    43

---
+++ Lesson 43 - Ключевое слово ref, что это и предназначение/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+/*
+ * Ключевое слово ref 
+ */
+
+/*
+ * Передача аргументов по ссылке 
+ */
+
+namespace Lesson_43___Ключевое_слово_ref__что_это_и_предназначение
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
