ed224e6e92a4f33f7cf7e1daaee1abd7fa3e83b5
Dormammu <saomuun@mail.ru>
2022-02-09 15:04:13 +0300

    44

---
+++ Lesson 44 - Ключевое слово out, разница out и ref/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+
+/*
+ * Ключевое слово out
+ * Разница между ref и out
+ */
+
+namespace Lesson_44___Ключевое_слово_out__разница_out_и_ref
+{
+    class Program
+    {
+        public static void Foo(ref int value)
+        {
+            value++;
+            System.Console.WriteLine(value);
+        }
+
+        //out - обязывает нас присваивать значение переменной внутри метода
+        public static void Bar(out int value)
+        {
+            value = 0;
+            System.Console.WriteLine(value);
+        }
+
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int aa;
+            Foo(ref a);
+            Bar(out aa);
+            string str = "3";
+            int.TryParse(str, out int result);
+            Bar(out int aaa);//сразу можем объявить внутри метода с out
+        }
+    }
+}
