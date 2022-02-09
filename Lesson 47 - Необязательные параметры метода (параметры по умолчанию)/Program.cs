0cba0681d3e8dd96819b32a0da1645a28540db75
Dormammu <saomuun@mail.ru>
2022-02-09 18:29:22 +0300

    47

---
+++ Lesson 47 - Необязательные параметры метода (параметры по умолчанию)/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+/*
+ * Необязательные параметры методов
+ */
+
+namespace Lesson_47___Необязательные_параметры_метода__параметры_по_умолчанию_
+{
+    class Program
+    {
+        //Параметры по умолчанию всегда должны быть в конце списка
+        private static int Sum(int a, int b, bool enableLogging = false)
+        {
+            int result = a + b;
+
+            if(enableLogging)
+            {
+                Console.WriteLine($"Значение переменной a = {a}");
+                Console.WriteLine($"Значение переменной b = {b}");
+                Console.WriteLine($"Значение переменной result = {result}");
+            }
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            Sum(2, 3);
+        }
+    }
+}
---
+++ Lesson 47 - Необязательные параметры метода (параметры по умолчанию)/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Lesson 47 - Необязательные параметры метода (параметры по умолчанию)")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lesson 47 - Необязательные параметры метода (параметры по умолчанию)")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("9b7f8691-e337-4ca4-9e24-46ef69803734")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
