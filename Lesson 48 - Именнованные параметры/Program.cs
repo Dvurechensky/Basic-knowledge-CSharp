4d974411c29bd96d7cb80a0bc6d9a8c6dfba78b8
Dormammu <saomuun@mail.ru>
2022-02-09 22:49:15 +0300

    48 49

---
+++ Lesson 48 - Именнованные параметры/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+/*
+ * Именнованные параметры
+ */
+
+namespace Lesson_48___Именнованные_параметры
+{
+    class Program
+    {
+        //Параметры по умолчанию всегда должны быть в конце списка
+        private static int Sum(int a, int b, bool enableLogging = false)
+        {
+            int result = a + b;
+
+            if (enableLogging)
+            {
+                Console.WriteLine($"Значение переменной a = {a}");
+                Console.WriteLine($"Значение переменной b = {b}");
+                Console.WriteLine($"Значение переменной result = {result}");
+            }
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            Sum(a: 1, b: 2);
+
+            int firstValue = 2;
+            int secondValue = 3;
+
+            //Чтобы внести ясность тому что выполнит функция 
+            Sum(a: firstValue, b: secondValue, enableLogging: true);
+        }
+    }
+}
---
+++ Lesson 49 - Что такое рекурсия, переполнение стека/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+/*
+ * Рекурсия
+ * 
+ * Методы и стек
+ * 
+ * Переполнение стека
+ */
+
+ /*
+  * ДЗ 1
+  * Реализовать вывод массива с помощью рекурсии
+  * 
+  * ДЗ 2
+  * Найти сумму элементов массива с помощью рекурсии
+  * 
+  * ДЗ 3
+  * Найти сумму цифр числа с помощью рекурсии Например - 561(число) = 12(сумма его чисел)
+  */
+
+namespace Lesson_49___Что_такое_рекурсия__переполнение_стека
+{
+    class Program
+    {
+        class Item
+        {
+            public int Value { get; set; }
+            public Item Child { get; set; }
+        }
+
+
+        static Item InitItem()
+        {
+            return new Item()
+            {
+                Value = 5,
+                Child = new Item()
+                {
+                    Value = 5,
+                    Child = new Item()
+                    {
+                        Value = 2
+                    }
+                }
+            };
+        }
+
+        static void Print(Item item)
+        {
+            if(item != null)
+            {
+                Console.WriteLine(item.Value);
+                Print(item.Child);
+            }
+        }
+
+        //Когда метод вызывает сам себя - рекурсия
+        //Обязательно нужно предусматривать условие выхода из рекурсии
+        //Недостаток - использовать можно только для коротких операций с малым числом переменных
+        private static void Foo(int i)
+        {
+            Console.WriteLine(i);
+            if (i >= 3)
+                return;
+            i++;
+            Foo(i);
+        }
+
+        static void Main(string[] args)
+        {
+            Foo(0);
+
+            Item item = InitItem();
+
+            Print(item);
+            //или
+            for (Item i = item; i != null; i = i.Child)
+            {
+                Console.WriteLine(i.Value);
+            }
+        }
+    }
+}
