ff5eb38aabe38c62ebcbe647b69c73a88d05f404
Dormammu <saomuun@mail.ru>
2022-02-13 22:20:55 +0300

    53 54

---
+++ Lesson 53 - var . Это не тип данных. Неявно типизированные переменные/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+/*
+ * Ключевое слово var
+ * 
+ * Неявно типизированные локальные переменные
+ */
+
+namespace Lesson_53___var.Это_не_тип_данных.Неявно_типизированные_переменные
+{
+    class Program
+    {
+        static int Sum_Int(int a, int b)
+        {
+            int aa = a + b;
+            return aa;
+        }
+        static int Sum_Var(int a, int b)
+        {
+            var aa = a + b;
+            return aa;
+        }
+
+        static void Main(string[] args)
+        {
+            var u = 5;
+
+            //Когда полезен?
+            //1 - сократить длинну кода
+            var dict = new Dictionary<int, string>();
+            var list = new List<int>();
+            //2 - создать анонимный тип данных
+            var obj = new { Name = "Мартин", Age = 25 };
+
+            //3 - использовать при выборке элементов в массиве
+            int[] numbers = { 1, 2, 3, 2, 4, 5 };
+            var result = from i in numbers where i > 1 select i;
+            foreach(var item in result)
+                Console.WriteLine($"{item}\t");
+
+
+            Stopwatch sw = Stopwatch.StartNew();
+            //4 - когда нужно получить результат выполнения метода
+            for (int i = 0; i < int.MaxValue; i++)
+            {
+                var sum_int = Sum_Int(1, i);
+            }
+            sw.Stop();
+            Console.WriteLine($"Sum_Int {sw.ElapsedMilliseconds}");
+            sw.Restart();
+            for (int i = 0; i < int.MaxValue; i++)
+            {
+                var sum_var = Sum_Var(1, i);
+            }
+            sw.Stop();
+            Console.WriteLine($"Sum_Var {sw.ElapsedMilliseconds}");
+
+
+            /*
+             * Не может быть спользовано параметром метода
+             * static void Bar (var a) 
+             * Не может быть типом возвращаемого значения
+             * static var Foo()
+             * Не может быть полями класса
+             * class MyClass { public var a}
+             */
+
+            Console.WriteLine();
+            Console.WriteLine(u.GetType());
+        }
+    }
+}
---
+++ Lesson 54 - enum, что это, и для чего нужно - перечисления/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+
+/*
+ * enum (перечисления)
+ */
+
+namespace Lesson_54___enum__что_это__и_для_чего_нужно___перечисления
+{
+    class Program
+    {
+        enum DayOfWeek : byte
+        {
+            Monday = 1,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+
+        enum Color
+        {
+            White,
+            Red,
+            Green,
+            Blue,
+            Orange
+        }
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                ConsoleKey key = Console.ReadKey().Key;
+
+                int keyCode = (int)key;
+
+                Console.WriteLine($"\tEnum {key} \tKey Code {keyCode}");
+
+                if (key == ConsoleKey.Enter)
+                    Console.WriteLine("Enter");
+
+                DayOfWeek dayOfWeek = DayOfWeek.Monday;
+
+                //получаем тип данных
+                Console.WriteLine(Enum.GetUnderlyingType(typeof(DayOfWeek)));
+
+                Console.WriteLine(dayOfWeek);
+
+                //приводим dayofweek к int
+                Console.WriteLine((int)dayOfWeek);
+                //приводим dayofweek к Dayofweek к int
+                int value = 3;
+                //Проверяем существования дня под таким номером в перечислении
+                if(Enum.IsDefined(typeof(DayOfWeek), value))
+                    Console.WriteLine((DayOfWeek)value);
+                //получаем следующий день
+                DayOfWeek nextday = GetNextDay(dayOfWeek);
+                Console.WriteLine(nextday);
+
+                //получить и вывести сразу все значения перечисления
+                var values = Enum.GetValues(typeof(Color));
+
+                foreach (var item in values)
+                {
+                    Console.WriteLine(item);
+                }
+
+                //перечисления можно парсить
+                string str = Console.ReadLine();
+                //ignoreCase - игнорировать большие и маленькие буквы
+                Color color = (Color)Enum.Parse(typeof(Color), str, ignoreCase: true);
+                Console.WriteLine(color);
+
+                //ну и фишка которая известна из начальных уроков
+
+                switch (Color)
+                {
+                    case Color.White:
+                        break;
+                    case Color.Red:
+                        break;
+                    case Color.Green:
+                        break;
+                    case Color.Blue:
+                        break;
+                    case Color.Orange:
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// получаем следующий день
+        /// </summary>
+        /// <param name="day"></param>
+        /// <returns></returns>
+        public static DayOfWeek GetNextDay(DayOfWeek day)
+        {
+            if (day < DayOfWeek.Sunday)
+                return day + 1;
+
+            return DayOfWeek.Monday;
+        }
+    }
+}
