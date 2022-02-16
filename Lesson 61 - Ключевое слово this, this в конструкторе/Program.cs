339042080d4e701e8f94f703c746c62fc605a385
Dormammu <saomuun@mail.ru>
2022-02-16 09:43:21 +0300

    61

---
+++ Lesson 61 - Ключевое слово this, this в конструкторе/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+/*
+ * this
+ */
+
+namespace Lesson_61___Ключевое_слово_this__this_в_конструкторе
+{
+    // -  this - можно получить доступ к текущему экземпляру класса
+    // - применяет когда есть не однозначность в именах переменных конструктора и полей класса
+    class Student
+    {
+        public Student(string lastName, DateTime birthday)
+        {
+            this.lastName = lastName;
+            this.birthday = birthday;
+        }
+
+        public Student(string lastName, string firstName, string middleName, DateTime birthday) : this(lastName, birthday)
+        {
+            this.firstName = firstName;
+            this.middleName = middleName;
+        }
+
+        public Student(Student student)
+        {
+            firstName = student.firstName;
+            lastName = student.lastName;
+            middleName = student.middleName;
+            birthday = student.birthday;
+        }
+
+        private string firstName;
+        private string middleName;
+        private string lastName;
+        private DateTime birthday;
+
+        public void SetLastName(string lastName)
+        {
+            this.lastName = lastName;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Имя: " + firstName +
+                $"\nФамилия: " + lastName +
+                $"\nОтчество: " + middleName +
+                $"\nДата рождения: " + birthday);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student_1 = new Student("Qumo", new DateTime(2000, 10, 5));
+            student_1.Print();
+        }
+    }
+}
