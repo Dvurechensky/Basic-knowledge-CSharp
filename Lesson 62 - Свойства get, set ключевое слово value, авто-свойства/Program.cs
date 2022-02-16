72d608ff9999aa437e97c582c7f1bd6a8da1d92e
Dormammu <saomuun@mail.ru>
2022-02-16 14:51:20 +0300

    62

---
+++ Lesson 62 - Свойства get, set ключевое слово value, авто-свойства/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+/*
+ * Сыойства (Properties)
+ * 
+ * Автоматические свойства
+ */
+
+namespace Lesson_62___Свойства_get__set_ключевое_слово_value__авто_свойства
+{
+    class Point
+    {
+        private int x;
+        public void SetX(int x)
+        {
+            this.x = x;
+        }
+
+        public int GetX()
+        {
+            return x;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point point = new Point();
+            point.SetX(5);
+            Console.WriteLine(point.GetX());
+        }
+    }
+}
