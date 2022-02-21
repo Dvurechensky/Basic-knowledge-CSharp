47f332ca3e8c7e17648dc9ce1779536918e3f0a6
Dormammu <saomuun@mail.ru>
2022-02-21 09:01:07 +0300

    64

---
+++ Lesson 64 - Статические методы, свойства, работа static/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+/*
+ * Ключесвое слово static
+ * 
+ * Статические методы класса
+ * 
+ * Статические свойства класса
+ */
+
+namespace Lesson_64___Статические_методы__свойства__работа_static
+{
+    class MyClass
+    {
+        private static int a;
+
+        private int b;
+
+        public static Foo()
+        {
+
+        }
+
+        public void Bar()
+        {
+
+        }
+
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass();
+            myClass.Bar();
+        } 
+    }
+}
