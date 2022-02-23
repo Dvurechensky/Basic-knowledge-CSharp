1e14da7159130f9a9d14e2b00247943a4c8a53b2
Dormammu <saomuun@mail.ru>
2022-02-24 00:29:50 +0300

    65

---
+++ Lesson 65 - Статический конструктор, как работает ключ. слово static/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+/*
+ * Ключевое слово static
+ * 
+ * Статический конструктор
+ */
+
+namespace Lesson_65___Статический_конструктор__как_работает_ключ.слово_static
+{
+    class Terro
+    {
+        public Terro()
+        {
+            Console.WriteLine("Конструктор");
+        }
+
+        public Terro(int a)
+        {
+            Console.WriteLine("Конструктор new a");
+        }
+
+        //Модификаторы доступа вообще не используются
+        //Может быть только один статический конструктор в классе
+        //Нельзя использовать перегрузку
+        //Не может принимать параметров
+        //Выполняется только один раз
+        static Terro()
+        {
+            Console.WriteLine("Статический конструктор");
+        }
+
+        public static void Foo()
+        {
+            Console.WriteLine("Foo");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Terro terro = new Terro();
+            new Terro();
+            new Terro();
+            new Terro();
+        }
+    }
+}
